eda0770c51b9da635371809d923bec9f27d0a95c
Helio S. Junior <helio.junior@codeminer42.com>
2018-11-03 13:27:32 -0300

    Done the scroll weight with callbacks and fixs positions

--- lib/components/weightCard.dart
+++ lib/components/weightCard.dart
@@ -1,21 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:bmi_calculator/utils/widget_utils.dart';
 import 'package:bmi_calculator/components/cardTitle.dart';
+import 'package:bmi_calculator/components/weightCard/weightBackground.dart';
+import 'package:bmi_calculator/components/weightCard/weightSlider.dart';
 
 class WeightCard extends StatefulWidget {
+  int initialWeight;
+  WeightCard({this.initialWeight});
+
   @override
   _WeightCardState createState() => _WeightCardState();
 }
 
 class _WeightCardState extends State<WeightCard> {
+  int weight;
+
+  Widget _drawSlider() {
+    return WeightBackground(
+      child: LayoutBuilder(
+        builder: (context, constraints) => constraints.isTight ? Container() : WeightSlider(
+          minValue: 30,
+          maxValue: 110,
+          width: constraints.maxWidth,
+          value: weight,
+          onChanged: (value) {
+            print('onChanged actual value: ${value.toString()}');
+            setState(() => weight = value);
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    weight = widget.initialWeight ?? 70;
+  }
+
   @override
   Widget build(BuildContext context) => Card(
     child: Padding(
       padding: EdgeInsets.only(top: screenAwareSize(32.0, context)),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           CardTitle('WEIGHT', subtitle: '(kg)'),
-          Container()
+          Expanded(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: _drawSlider(),
+              ),
+            ),
+          ),
         ],
       ),
     ),
